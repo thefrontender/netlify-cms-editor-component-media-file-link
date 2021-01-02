54f1dca543c90619435bf60817ef3d907c36cfc4
Jason Sheehy <jsheehy@atlassian.com>
2021-01-02 20:07:57 +1100

    fix: publish UMD javascript payload

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = function (api) {
 	api.cache(true);
 
 	const presets = [ "@babel/preset-env", "@babel/preset-react" ];
-	const plugins = [ ];
+	const plugins = [ "@babel/plugin-transform-modules-umd" ];
 
 	return {
 		presets,
